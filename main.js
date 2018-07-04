fd65209e547f091eb315535fffd58ffee0147a91
Andrew Gregory <andrewjgregoryajg@gmail.com>
2018-07-04 14:52:06 -0400

    search student, click first record that is returned

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
 const puppeteer = require("puppeteer");
 const CREDS = require("./creds");
 
-async function login() {
+async function main() {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.setViewport({ width: 1200, height: 1000 });
+  await login(page);
+  await searchStudent(page, "andrew gregory");
+  await clickStudentRecord(page);
+}
+
+async function clickStudentRecord(page) {
+  await page.click(".candidate-row");
+}
+
+async function searchStudent(page, name) {
+  await page.click("#candidate-search-box-gl");
+  await page.keyboard.type(name);
+  await page.keyboard.press("Enter");
+  await page.waitForNavigation({ waitUntil: "networkidle0" });
+}
+
+async function login(page) {
   await page.goto("https://www.hackerrank.com/work/login");
   await page.click("#email");
   await page.keyboard.type(CREDS.username);
@@ -17,4 +34,4 @@ async function login() {
   await page.waitForNavigation();
 }
 
-login();
+main();
