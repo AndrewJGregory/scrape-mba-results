0235c11c3c96384104b084603a6cf44f0ff8168d
Andrew Gregory <andrewjgregoryajg@gmail.com>
2018-07-06 14:24:00 -0400

    grab mba pdfs correctly

--- zip.js
+++ zip.js
@@ -2,15 +2,19 @@ const fs = require("fs");
 const archiver = require("archiver");
 
 const zipMBAresults = (name, email) => {
-  const output = fs.createWriteStream(`${name}_MBA_results.zip`);
+  const output = fs.createWriteStream(
+    `${name.replace(" ", "_")}_MBA_results.zip`
+  );
 
   const archive = archiver("zip", {
     zlib: { level: 9 }
   });
 
   archive.pipe(output);
-
-  archive.glob(`Report_MBA__*${email}*.pdf`, {
+  // hackerrank does not include the EXACT email address in the downloaded filename
+  // instead, it replaces the '@' with a '_'
+  const formattedEmail = email.replace("@", "_");
+  archive.glob(`Report_MBA__*${formattedEmail}*.pdf`, {
     cwd: "/Users/andrewgregory/Downloads"
   });
 
