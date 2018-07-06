3cd768c5db14b3f367e7ac40d0bdb5402556c542
Andrew Gregory <andrewjgregoryajg@gmail.com>
2018-07-06 15:42:06 -0400

    send email with attached mba results zip

---
+++ mail.js
@@ -0,0 +1,33 @@
+const nodemailer = require("nodemailer");
+const CREDS = require("./CREDS");
+
+const emailMBAzip = (name, email) => {
+  const fileName = name.replace(" ", "_") + "_MBA_results.zip";
+  const mailOptions = {
+    from: `${CREDS["email"]}`,
+    to: `${email}`,
+    subject: "MBA results",
+    attachments: [
+      {
+        path: `/Users/andrewgregory/Desktop/repos/scrape-mba-results/${fileName}`
+      }
+    ]
+  };
+
+  transporter.sendMail(mailOptions, (err, info) => {
+    if (err) console.log(err);
+    else console.log(`Finished sending email to ${name}...`);
+  });
+};
+
+const openEmailConenction = () => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: `${CREDS["email"]}`,
+      pass: `${CREDS["email_password"]}`
+    }
+  });
+};
+
+module.exports = { emailMBAzip, openEmailConenction };
