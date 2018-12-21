b214583cb5049901117f51c638f2b2c7d8754b69
Andrew Gregory <andrewjgregoryajg@gmail.com>
2018-12-21 10:29:21 -0500

    format scores table email

--- mail.js
+++ mail.js
@@ -1,6 +1,56 @@
 const nodemailer = require("nodemailer");
 const { CREDS, PATHS } = require("./constants");
 
+const emailMBAscores = (student, transporter) => {
+  const { email, name, MBAs } = student;
+
+  MBAs.sort((mbaOne, mbaTwo) => {
+    const scoreOne = parseFloat(mbaOne.percentageScore.slice(0, -1));
+    const scoreTwo = parseFloat(mbaTwo.percentageScore.slice(0, -1));
+    return scoreOne < scoreTwo ? 1 : -1;
+  });
+
+  let starterHTML = `
+  <table style='text-align: center'>
+  <thead>
+  <tr>
+  <th colspan='3'>MBA scores</th>
+  </tr>
+  </thead>
+  <tbody style='text-align: center'>
+  <tr>
+  <td style='font-weight: 900'>Subject</td>
+  <td style='font-weight: 900'>Total Score</td>
+  <td style='font-weight: 900'>Percentage Score</td>
+  </tr>
+  `;
+
+  let html = MBAs.reduce((html, MBA) => {
+    const { percentageScore, totalScore, subject } = MBA;
+    let row = "<tr>";
+    const subjectCell = `<td>${subject}</td>`;
+    const totalCell = `<td>${totalScore}</td>`;
+    const percentageCell = `<td>${percentageScore}</td>`;
+    row = row
+      .concat(`${subjectCell}${totalCell}${percentageCell}`)
+      .concat("</tr>");
+    html = html.concat(row);
+    return html;
+  }, starterHTML);
+
+  const endingHTML = "</tbody></table>";
+  html = html.concat(endingHTML);
+
+  const mailOptions = {
+    from: `${CREDS["email"]["address"]}`,
+    to: `${email}, agregory@appacademy.io, dcatalano@appacademy.io, jfehrman@appacademy.io`,
+    subject: `MBA results for ${name}`,
+    html,
+  };
+
+  return transporter.sendMail(mailOptions);
+};
+
 const emailMBAzip = (name, email, transporter) => {
   const fileName = name.replace(" ", "_") + "_MBA_results.zip";
   const mailOptions = {
@@ -30,4 +80,4 @@ const openEmailConnection = () => {
   });
 };
 
-module.exports = { emailMBAzip, openEmailConnection };
+module.exports = { emailMBAzip, openEmailConnection, emailMBAscores };
