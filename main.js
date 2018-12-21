fb43da0499ffd793d0a6e19540e61fc9cf05c5ae
Andrew Gregory <andrewjgregoryajg@gmail.com>
2018-12-21 11:31:10 -0500

    send scores to individual students

--- main.js
+++ main.js
@@ -24,104 +24,17 @@ const writeStudentInfo = async (jobberWocky, platform) => {
   platform.writeToFile(newStudents);
 };
 
-function sendEmails() {
+const sendScores = async hackerRank => {
   const transporter = openEmailConnection();
   for (let i = 0; i < STUDENTS.length; i++) {
     const { name, email } = STUDENTS[i];
-    emailMBAzip(name, email, transporter);
+    await hackerRank.searchStudent(email);
+    await hackerRank.clickStudentRecord();
+    const MBAs = await hackerRank.getScores();
+    const student = { name, email, MBAs };
+    await emailMBAscores(student, transporter);
+    console.log(`sent email to ${name}`);
   }
-}
-
-function clearConsole() {
-  process.stdout.write("\033c");
-}
-
-async function startSession() {
-  let browser = await puppeteer.launch({ headless: false, devTools: true });
-  let page = await browser.newPage();
-  await page.setViewport({ width: 1200, height: 1000 });
-  await login(page);
-  return { browser, page };
-}
-
-async function downloadAllReports(page, email) {
-  await searchStudent(page, email);
-  await clickStudentRecord(page);
-  const allHrefs = await findAllHrefs(page);
-  for (let i = 0; i < allHrefs.length; i++) {
-    const href = allHrefs[i];
-    await gotoReportPage(page, href);
-    console.log(`Downloading report #${i + 1}...`);
-    await clickDownloadBtn(page);
-  }
-}
-
-async function findAllHrefs(page) {
-  await page.waitForSelector(".icon-keyboard");
-  const allhrefs = await page.$$eval(".js-backbone", links =>
-    links.reduce((hrefs, link) => {
-      if (link.innerText === "View Report") hrefs.push(link.href);
-      return hrefs;
-    }, []),
-  );
-  return allhrefs;
-}
-
-async function clickDownloadBtn(page) {
-  await page.waitForSelector(".icon2-download");
-  await page.evaluate(() => document.querySelector(".icon2-download").click());
-  await page.waitForSelector(".js-pdfprogress");
-  await page.waitForFunction(() =>
-    document
-      .querySelector(".js-pdfprogress")
-      .innerHTML.includes("Your report is ready."),
-  ),
-    await page.waitFor(1000);
-}
-
-async function gotoReportPage(page, href) {
-  await page.goto(`${href}`);
-}
-
-async function clickStudentRecord(page) {
-  await page.waitForSelector(".candidate-row", { visible: true });
-  await page.evaluate(() => document.querySelector(".candidate-row").click());
-}
-
-async function searchStudent(page, email) {
-  await page.waitForSelector("#candidate-search-box-gl");
-  await page.click("#candidate-search-box-gl");
-  await page.keyboard.type(email);
-  await page.keyboard.press("Enter");
-  await page.waitForNavigation({ waitUntil: "networkidle0" });
-}
-
-async function login(page) {
-  await page.goto("https://www.hackerrank.com/work/login");
-  await page.click("#email");
-  await page.keyboard.type(CREDS["HackerRank"]["username"]);
-
-  await page.click("#password");
-  await page.keyboard.type(CREDS["HackerRank"]["password"]);
-
-  await Promise.all([page.waitForNavigation(), page.click(".signupBtn")]);
-  await page
-    .click(".aurycModalCloseButton")
-    .catch(() => console.log("no modal found"));
-}
-
-function deleteMBAFiles() {
-  const directory = `${PATHS["download"]}`;
-  fs.readdir(directory, (err, fileNames) => {
-    for (const name of fileNames) {
-      const shouldDelete = name.includes("Report_MBA__");
-      if (shouldDelete) {
-        fs.unlink(path.resolve(directory, name), () =>
-          console.log(`Deleted ${name}`),
-        );
-      }
-    }
-  });
-}
+};
 
 main();
