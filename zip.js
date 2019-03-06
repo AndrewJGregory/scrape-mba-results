89feb203cb1e6ada99f8466fbcffe87ceda7ed95
Andrew Gregory <andrewjgregoryajg@gmail.com>
2019-03-06 14:26:16 -0500

    promisify zipping mba results

--- zip.js
+++ zip.js
@@ -2,24 +2,27 @@ const fs = require("fs");
 const archiver = require("archiver");
 const PATHS = require("./config/paths");
 
-const zipMBAresults = (name, email) => {
-  const output = fs.createWriteStream(
-    `${name.replace(" ", "_")}_MBA_results.zip`
-  );
+const zipMBAresults = async (name, email) => {
+  return new Promise(resolve => {
+    const output = fs.createWriteStream(
+      `${name.replace(" ", "_")}_MBA_results.zip`,
+    );
 
-  const archive = archiver("zip", {
-    zlib: { level: 9 }
-  });
+    const archive = archiver("zip", {
+      zlib: { level: 9 },
+    });
 
-  archive.pipe(output);
-  // hackerrank does not include the EXACT email address in the downloaded filename
-  // instead, it replaces the '@' with a '_'
-  const formattedEmail = email.replace("@", "_");
-  archive.glob(`Report_MBA__*${formattedEmail}*.pdf`, {
-    cwd: `${PATHS["download"]}`
-  });
+    archive.pipe(output);
+    // hackerrank does not include the EXACT email address in the downloaded filename
+    // instead, it replaces the '@' with a '_'
+    const formattedEmail = email.replace("@", "_");
+    archive.glob(`Report_MBA__*${formattedEmail}*.pdf`, {
+      cwd: `${PATHS["download"]}`,
+    });
 
-  archive.finalize();
+    archive.finalize();
+    resolve();
+  }).then(() => console.log(`created zip for ${name}`));
 };
 
 module.exports = zipMBAresults;
