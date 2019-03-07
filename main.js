6ba084490efaf116cd5944888d1f86d34c6dd1f1
Andrew Gregory <andrewjgregoryajg@gmail.com>
2019-03-07 17:29:12 -0500

    send diff emails for students with finished/unfinished MBAs

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
 const HackerRank = require("./hackerrank");
 const Platform = require("./platform");
 const STUDENTS = require("./config/students");
-const { openEmailConnection, emailMBAscores } = require("./mail");
+const {
+  openEmailConnection,
+  emailMBAzip,
+  sendReminderEmail,
+} = require("./mail");
+const zipMBAresults = require("./zip");
+
+const ALL_SUBJECTS = [
+  "JavaScript",
+  "React/Redux",
+  "General Computer Science",
+  "Coding Questions",
+  "Algorithms",
+  "Data Types/Structures",
+  "Rails/ActiveRecord",
+  "Security",
+  "Front End/Vanilla DOM manipulation",
+  "Object Oriented Programming",
+  "Databases",
+  "Ruby",
+  "Intermediate Programming Concepts",
+];
 
 const main = async () => {
   const platform = new Platform();
@@ -10,6 +31,21 @@ const main = async () => {
   const hackerRank = new HackerRank();
   hackerRank.page = page;
   await hackerRank.login();
+  const transporter = openEmailConnection();
+  for (let i = 0; i < STUDENTS.length; i++) {
+    const { name, email } = STUDENTS[i];
+    const finishedSubjects = await hackerRank.findFinishedSubjects(STUDENTS[i]);
+    const subjectsToDo = ALL_SUBJECTS.filter(
+      subj => !finishedSubjects.includes(subj),
+    );
+    if (subjectsToDo.length > 0) {
+      await sendReminderEmail(name, email, subjectsToDo, transporter);
+    } else {
+      await hackerRank.downloadAllReports(STUDENTS[i]);
+      await zipMBAresults(name, email);
+      await emailMBAzip(name, email, transporter);
+    }
+  }
   await browser.close();
 };
 
